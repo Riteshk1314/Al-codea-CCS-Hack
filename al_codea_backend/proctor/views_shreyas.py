dba15e2d942133bd633da78a00bff9aa1f42389a
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-02 21:31:25 +0000

    basic feild gathering

--- al_codea_backend/proctor/views_shreyas.py
+++ al_codea_backend/proctor/views_shreyas.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        data=request.POST
+        count=data.get('textarea_count')
+        fields=[]
+        for i in range(int(count)):
+            fields.append(data.get(f'field{i+1}'))
+        print(fields)
+        return redirect('/')
+    return render(request, 'index.html')
