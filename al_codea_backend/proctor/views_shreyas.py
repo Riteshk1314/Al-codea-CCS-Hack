49e35b774c48c6ec070658153d27e5318feca420
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-03 06:12:54 +0000

    corrected order

--- al_codea_backend/proctor/views_shreyas.py
+++ al_codea_backend/proctor/views_shreyas.py
@@ -2,12 +2,17 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
+    fields=[]
     if request.method == 'POST':
         data=request.POST
         count=data.get('textarea_count')
-        fields=[]
         for i in range(int(count)):
             fields.append(data.get(f'field{i+1}'))
         print(fields)
-        return redirect('/')
-    return render(request, 'index.html')
+        context={
+        'field':fields,
+        }
+        return render(request, 'index.html', context)
+        
+    
+    return render(request, 'index.html', {'field': fields})
