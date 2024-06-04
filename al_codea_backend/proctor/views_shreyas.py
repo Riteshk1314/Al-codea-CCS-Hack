57a7abb07d9f0bbf96ddc300c39c3945fef83021
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-04 18:09:02 +0000

    object creation

--- al_codea_backend/proctor/views_shreyas.py
+++ al_codea_backend/proctor/views_shreyas.py
@@ -6,52 +6,53 @@ from .models import *
 #admin
 #admin
 
+xyz="""1. What is the purpose of the `componentDidMount()` method in the context of React?
+a) It is used to render a component
+b) It is used to update a component
+c) It is used to handle events
+d) It is used to run code after a component is mounted to the DOM
 
-# data="""1. What is the purpose of the `componentDidMount()` method in the context of React?
-# a) It is used to render a component
-# b) It is used to update a component
-# c) It is used to handle events
-# d) It is used to run code after a component is mounted to the DOM
+Answer: d) It is used to run code after a component is mounted to the DOM
 
-# Answer: d) It is used to run code after a component is mounted to the DOM
+2. Which of the following is used to pass data from a parent component to a child component in React?
+a) state
+b) props
+c) refs
+d) keys
 
-# 2. Which of the following is used to pass data from a parent component to a child component in React?
-# a) state
-# b) props
-# c) refs
-# d) keys
+Answer: b) props
 
-# Answer: b) props
+3. What is the role of the `key` prop in the following code snippet?
+```php    
+<tr key={index}>
+  ...
+</tr>
+```
+a) It is used to identify the row uniquely
+b) It is used to style the row
+c) It is used to handle events on the row
+d) It is used to pass data to the row
 
-# 3. What is the role of the `key` prop in the following code snippet?
-# ```php    
-# <tr key={index}>
-#   ...
-# </tr>
-# ```
-# a) It is used to identify the row uniquely
-# b) It is used to style the row
-# c) It is used to handle events on the row
-# d) It is used to pass data to the row
+Answer: a) It is used to identify the row uniquely
 
-# Answer: a) It is used to identify the row uniquely
+4. Which of the following is used to remove an item from an array in the `removeCharacter()` method?
+a) filter()
+b) map()
+c) reduce()
+d) forEach()
 
-# 4. Which of the following is used to remove an item from an array in the `removeCharacter()` method?
-# a) filter()
-# b) map()
-# c) reduce()
-# d) forEach()
+Answer: a) filter()
 
-# Answer: a) filter()
+5. Which of the following is used to compile and create a production build of a React application?
+a) npm run build
+b) npm run compile
+c) npm run production
+d) npm run create-build
 
-# 5. Which of the following is used to compile and create a production build of a React application?
-# a) npm run build
-# b) npm run compile
-# c) npm run production
-# d) npm run create-build
+Answer: a) npm run build
+"""
 
-# Answer: a) npm run build
-# """
+    
 
 
 
@@ -95,8 +96,8 @@ def field_creation(request):
     return render(request,'index.html')
 
 
-def object_creation(question,options,answer):
-    field="python"
+def object_creation(question,options,answer,field):
+    field=field
     question=question
     option1=options[0],
     option2=options[1],
@@ -116,36 +117,11 @@ def object_creation(question,options,answer):
         )
 
 
-def extractor(onequestion):
-    options=[]
-    answer=''
-    questions=''
-    ques=''
-    lines = onequestion.split('\n')
-    
-    for line in lines:
-        if line.startswith('a)') or line.startswith('b)') or line.startswith('c)') or line.startswith('d)'):
-            options.append(line.strip())
-        elif line.startswith('Answer:'):
-            answer=line
-        else:
-            ques+=line
-            ques+='\n'
-    questions=ques
-    for a in questions:
-        print(a)
-    for b in options:
-        print(b)
-    for c in answer:
-        print(c)
-    
-    object_creation(questions,options,answer)
 
 
-
-
-def question_creation(data):
-    
+def question_creation(prompt,field):
+    field=field
+    data=prompt
     total_questions = [] #the whole string recieved from langchain
     question = ''
     word = ''
@@ -165,8 +141,41 @@ def question_creation(data):
             word = ''
         question += char
         
+        
+        
+
     for i in total_questions:
-        extractor(i)
+        options=[]
+        answer=''
+        ques=''
+        lines = i.split('\n')
+        
+        for line in lines:
+            if line.startswith('a)') or line.startswith('b)') or line.startswith('c)') or line.startswith('d)'):
+                options.append(line.strip())
+            elif line.startswith('Answer:'):
+                answer=line
+            else:
+                ques+=line
+                ques+='\n'
+        question=ques
+        # print(question)
+        # for b in options:
+        #     print(b)
+        # print(answer)
+    
+        object_creation(question,options,answer,field)
+        # if not Question.objects.filter(field=Topic.objects.get(topic=field),question=question).exists():
+        #     Question.objects.create(
+        #         field=Topic.objects.get(topic=field),
+        #         question=question,
+        #         option1=options[0],
+        #         option2=options[1],
+        #         option3=options[2],
+        #         option4=options[3],
+        #         answer=answer
+        #     )
+   
     
     
    
@@ -175,17 +184,14 @@ def question_creation(data):
 
 def index(request):
     fields=[]
-    question=[]
-    answer=[]
     if request.method == 'POST':
         data=request.POST
         count=data.get('textarea_count')
         for i in range(int(count)):
-            fields.append(data.get(f'topic{i}'))
+            fields.append(data.get(f'field{i+1}'))
+            
+        question_creation(xyz,"dsa")
         
-        
-        
-        return redirect('/')
     
     
     querryset=Topic.objects.all()
