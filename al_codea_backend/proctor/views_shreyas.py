743e3df61872ec143bc0d53bf75deb63f9607379
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-04 14:36:45 +0000

    question handeling

--- al_codea_backend/proctor/views_shreyas.py
+++ al_codea_backend/proctor/views_shreyas.py
@@ -7,6 +7,58 @@ from .models import *
 #admin
 
 
+# data="""1. What is the purpose of the `componentDidMount()` method in the context of React?
+# a) It is used to render a component
+# b) It is used to update a component
+# c) It is used to handle events
+# d) It is used to run code after a component is mounted to the DOM
+
+# Answer: d) It is used to run code after a component is mounted to the DOM
+
+# 2. Which of the following is used to pass data from a parent component to a child component in React?
+# a) state
+# b) props
+# c) refs
+# d) keys
+
+# Answer: b) props
+
+# 3. What is the role of the `key` prop in the following code snippet?
+# ```php    
+# <tr key={index}>
+#   ...
+# </tr>
+# ```
+# a) It is used to identify the row uniquely
+# b) It is used to style the row
+# c) It is used to handle events on the row
+# d) It is used to pass data to the row
+
+# Answer: a) It is used to identify the row uniquely
+
+# 4. Which of the following is used to remove an item from an array in the `removeCharacter()` method?
+# a) filter()
+# b) map()
+# c) reduce()
+# d) forEach()
+
+# Answer: a) filter()
+
+# 5. Which of the following is used to compile and create a production build of a React application?
+# a) npm run build
+# b) npm run compile
+# c) npm run production
+# d) npm run create-build
+
+# Answer: a) npm run build
+# """
+
+
+
+
+
+
+
 all_feilds=[
     'c',
     'java',
@@ -43,18 +95,14 @@ def field_creation(request):
     return render(request,'index.html')
 
 
-
-
-def question_creation(request):
+def object_creation(question,options,answer):
     field="python"
-    question='1. What is the purpose of the `componentDidMount()` method in the context of React?',
-    option1='It is used to render a component',
-    option2='It is used to update a component',
-    option3='It is used to handle events',
-    option4='It is used to run code after a component is mounted to the DOM',
-    answer=option4
-    
-    
+    question=question
+    option1=options[0],
+    option2=options[1],
+    option3=options[2],
+    option4=options[3],
+    answer=answer
     
     if not Question.objects.filter(field=Topic.objects.get(topic=field),question=question).exists():
         Question.objects.create(
@@ -66,7 +114,62 @@ def question_creation(request):
             option4=option4,
             answer=answer
         )
-    return render(request,'index.html')
+
+
+def extractor(onequestion):
+    options=[]
+    answer=''
+    questions=''
+    ques=''
+    lines = onequestion.split('\n')
+    
+    for line in lines:
+        if line.startswith('a)') or line.startswith('b)') or line.startswith('c)') or line.startswith('d)'):
+            options.append(line.strip())
+        elif line.startswith('Answer:'):
+            answer=line
+        else:
+            ques+=line
+            ques+='\n'
+    questions=ques
+    for a in questions:
+        print(a)
+    for b in options:
+        print(b)
+    for c in answer:
+        print(c)
+    
+    object_creation(questions,options,answer)
+
+
+
+
+def question_creation(data):
+    
+    total_questions = [] #the whole string recieved from langchain
+    question = ''
+    word = ''
+    is_answer = False
+
+    for char in data:
+        word += char
+        if char == ' ':
+            if word == 'Answer: ':
+                is_answer = True
+                word = ''
+        if char == '\n':
+            if is_answer:
+                total_questions.append(question.strip())
+                question = ''
+                is_answer = False
+            word = ''
+        question += char
+        
+    for i in total_questions:
+        extractor(i)
+    
+    
+   
 
 
 
