ca19968627a1f3742ee7e094c5c86b50d62cb085
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-06-05 13:07:50 +0530

    langchain done. lang graph done, students remaining push,  langgraph trial

--- al_codea_backend/proctor/models.py
+++ al_codea_backend/proctor/models.py
@@ -1,16 +1,16 @@
 
 
-# class QuizQuestionpy(models.Model):
+class QuizQuestionpy(models.Model):
     
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     question_text = models.TextField()
-#     option_a = models.TextField()
-#     option_b = models.TextField()
-#     option_c = models.TextField()
-#     option_d = models.TextField()
-#     correct_option = models.CharField(max_length=1, choices=[('A'),('B'),('C'),('D'),])
-#     def __str__(self):
-#         return self.question_text
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_text = models.TextField()
+    option_a = models.TextField()
+    option_b = models.TextField()
+    option_c = models.TextField()
+    option_d = models.TextField()
+    correct_option = models.CharField(max_length=1, choices=[('A'),('B'),('C'),('D'),])
+    def __str__(self):
+        return self.question_text
     
 
 from django.db import models
