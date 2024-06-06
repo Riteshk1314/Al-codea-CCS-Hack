d8930e440e4be28092a9437771fe0945002cdebb
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-06 14:41:12 +0000

    working frontend StudentPage

--- frontend/src/components/StudentPage.js
+++ frontend/src/components/StudentPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React from 'react';
 import './StudentPage.css';
 
 const StudentPage = () => {
@@ -315,40 +314,40 @@ return (
     </div>
   );
 };
-function StudentPage({ details }) {
-  // Check if details is undefined or empty
-  if (!details || details.length === 0) {
-    return (
-      <div className="student-page">
-        <h1>Student Page</h1>
-        <div>Loading...</div>
-      </div>
-    );
-  }
+// function StudentPage({ details }) {
+//   // Check if details is undefined or empty
+//   if (!details || details.length === 0) {
+//     return (
+//       <div className="student-page">
+//         <h1>Student Page</h1>
+//         <div>Loading...</div>
+//       </div>
+//     );
+//   }
 
-  // If details is available, render the component with the data
-  return (
-    <div className="student-page">
-      <h1>Student Page</h1>
-      <div>
-        <h1>Details</h1>
-        <ul>
-          {details.map((detail, index) => (
-            <li key={index}>
-              <p>{detail.question}</p>
-              <p>{detail.option1}</p>
-              <p>{detail.option2}</p>
-              <p>{detail.option3}</p>
-              <p>{detail.option4}</p>
-              <p>{detail.answer}</p>
+//   // If details is available, render the component with the data
+//   return (
+//     <div className="student-page">
+//       <h1>Student Page</h1>
+//       <div>
+//         <h1>Details</h1>
+//         <ul>
+//           {details.map((detail, index) => (
+//             <li key={index}>
+//               <p>{detail.question}</p>
+//               <p>{detail.option1}</p>
+//               <p>{detail.option2}</p>
+//               <p>{detail.option3}</p>
+//               <p>{detail.option4}</p>
+//               <p>{detail.answer}</p>
 
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default StudentPage;
 
