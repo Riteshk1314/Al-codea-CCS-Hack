a44356cb6c1ca562562423634b3783a590e57914
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-07 10:55:21 +0000

    responsive CameraFeed

--- frontend/src/components/StudentPage.js
+++ frontend/src/components/StudentPage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './StudentPage.css';
-import CameraFeed from './CameraFeed';
+import React, { useState, useEffect } from "react";
+import "./StudentPage.css";
+import CameraFeed from "./CameraFeed";
 
 const StudentPage = ({ details }) => {
   const [timeLeft, setTimeLeft] = useState(900); // 15 minutes timer
@@ -10,14 +10,14 @@ const StudentPage = ({ details }) => {
     2: { seen: false, answered: false, review: false },
     3: { seen: false, answered: false, review: false },
     4: { seen: false, answered: false, review: false },
-    5: { seen: false, answered: false, review: false }
+    5: { seen: false, answered: false, review: false },
   });
   const [selectedAnswers, setSelectedAnswers] = useState({
-    1: '',
-    2: '',
-    3: '',
+    1: "",
+    2: "",
+    3: "",
     4: [],
-    5: []
+    5: [],
   });
 
   useEffect(() => {
@@ -31,7 +31,7 @@ const StudentPage = ({ details }) => {
     setQuestionStatus((prevState) => ({
       ...prevState,
       [currentQuestion]: { ...prevState[currentQuestion], seen: true },
-      [questionNumber]: { ...prevState[questionNumber], seen: true }
+      [questionNumber]: { ...prevState[questionNumber], seen: true },
     }));
     setCurrentQuestion(questionNumber);
   };
@@ -39,18 +39,21 @@ const StudentPage = ({ details }) => {
   const handleMarkForReview = () => {
     setQuestionStatus((prevState) => ({
       ...prevState,
-      [currentQuestion]: { ...prevState[currentQuestion], review: !prevState[currentQuestion].review }
+      [currentQuestion]: {
+        ...prevState[currentQuestion],
+        review: !prevState[currentQuestion].review,
+      },
     }));
   };
 
   const handleOptionChange = (questionNumber, answer) => {
     setSelectedAnswers((prevState) => ({
       ...prevState,
-      [questionNumber]: answer
+      [questionNumber]: answer,
     }));
     setQuestionStatus((prevState) => ({
       ...prevState,
-      [questionNumber]: { ...prevState[questionNumber], answered: true }
+      [questionNumber]: { ...prevState[questionNumber], answered: true },
     }));
   };
 
@@ -73,12 +76,12 @@ const StudentPage = ({ details }) => {
         : [...prevState[questionNumber], answer];
       return {
         ...prevState,
-        [questionNumber]: newAnswers
+        [questionNumber]: newAnswers,
       };
     });
     setQuestionStatus((prevState) => ({
       ...prevState,
-      [questionNumber]: { ...prevState[questionNumber], answered: true }
+      [questionNumber]: { ...prevState[questionNumber], answered: true },
     }));
   };
 
@@ -96,12 +99,12 @@ const StudentPage = ({ details }) => {
   return (
     <div className="test-container">
       <button className="nav-toggle" onClick={handleToggleNav}>
-        {isNavOpen ? '☰' : '☰'}
+        {isNavOpen ? "☰" : "☰"}
       </button>
-      <div className={`right-section ${isNavOpen ? 'open' : ''}`}>
-        <div className="camera-feed-container">
-          <CameraFeed />
-        </div>
+      <div className="camera-feed-container">
+        <CameraFeed />
+      </div>
+      <div className={`right-section ${isNavOpen ? "open" : ""}`}>
         <div className="info-box">
           <label>Time Left: </label>
           <span className="time-left">{timeLeft} seconds</span>
@@ -126,20 +129,33 @@ const StudentPage = ({ details }) => {
           {Object.keys(questionStatus).map((questionNumber) => (
             <button
               key={questionNumber}
-              className={`nav-button ${questionStatus[questionNumber].answered ? 'answered' : ''} ${questionStatus[questionNumber].seen ? 'seen' : ''} ${questionStatus[questionNumber].review ? 'review' : ''} ${currentQuestion === parseInt(questionNumber) ? 'active' : ''}`}
+              className={`nav-button ${
+                questionStatus[questionNumber].answered ? "answered" : ""
+              } ${questionStatus[questionNumber].seen ? "seen" : ""} ${
+                questionStatus[questionNumber].review ? "review" : ""
+              } ${
+                currentQuestion === parseInt(questionNumber) ? "active" : ""
+              }`}
               onClick={() => handleQuestionChange(parseInt(questionNumber))}
             >
               {questionNumber}
-              {questionStatus[questionNumber].answered && questionStatus[questionNumber].review && <span className="review-dot"></span>}
+              {questionStatus[questionNumber].answered &&
+                questionStatus[questionNumber].review && (
+                  <span className="review-dot"></span>
+                )}
             </button>
           ))}
         </div>
         <button className="review-button" onClick={handleMarkForReview}>
-          {questionStatus[currentQuestion].review ? 'Unmark Review' : 'Mark for Review'}
+          {questionStatus[currentQuestion].review
+            ? "Unmark Review"
+            : "Mark for Review"}
         </button>
-        <button className="submit-button" onClick={handleSubmit}>Submit Test</button>
+        <button className="submit-button" onClick={handleSubmit}>
+          Submit Test
+        </button>
       </div>
-      <div className={`question-section ${isNavOpen ? 'nav-open' : ''}`}>
+      <div className={`question-section ${isNavOpen ? "nav-open" : ""}`}>
         {currentQuestion === 1 && (
           <div className="question">
             <label>1. Question one text here?</label>
@@ -150,8 +166,8 @@ const StudentPage = ({ details }) => {
                   id="q1a1"
                   name="q1"
                   value="A"
-                  checked={selectedAnswers[1] === 'A'}
-                  onChange={() => handleOptionChange(1, 'A')}
+                  checked={selectedAnswers[1] === "A"}
+                  onChange={() => handleOptionChange(1, "A")}
                 />
                 <label htmlFor="q1a1">Option A</label>
               </div>
@@ -161,8 +177,8 @@ const StudentPage = ({ details }) => {
                   id="q1a2"
                   name="q1"
                   value="B"
-                  checked={selectedAnswers[1] === 'B'}
-                  onChange={() => handleOptionChange(1, 'B')}
+                  checked={selectedAnswers[1] === "B"}
+                  onChange={() => handleOptionChange(1, "B")}
                 />
                 <label htmlFor="q1a2">Option B</label>
               </div>
@@ -179,8 +195,8 @@ const StudentPage = ({ details }) => {
                   id="q2a1"
                   name="q2"
                   value="A"
-                  checked={selectedAnswers[2] === 'A'}
-                  onChange={() => handleOptionChange(2, 'A')}
+                  checked={selectedAnswers[2] === "A"}
+                  onChange={() => handleOptionChange(2, "A")}
                 />
                 <label htmlFor="q2a1">Option A</label>
               </div>
@@ -190,8 +206,8 @@ const StudentPage = ({ details }) => {
                   id="q2a2"
                   name="q2"
                   value="B"
-                  checked={selectedAnswers[2] === 'B'}
-                  onChange={() => handleOptionChange(2, 'B')}
+                  checked={selectedAnswers[2] === "B"}
+                  onChange={() => handleOptionChange(2, "B")}
                 />
                 <label htmlFor="q2a2">Option B</label>
               </div>
@@ -208,8 +224,8 @@ const StudentPage = ({ details }) => {
                   id="q3a1"
                   name="q3"
                   value="A"
-                  checked={selectedAnswers[3] === 'A'}
-                  onChange={() => handleOptionChange(3, 'A')}
+                  checked={selectedAnswers[3] === "A"}
+                  onChange={() => handleOptionChange(3, "A")}
                 />
                 <label htmlFor="q3a1">Option A</label>
               </div>
@@ -219,8 +235,8 @@ const StudentPage = ({ details }) => {
                   id="q3a2"
                   name="q3"
                   value="B"
-                  checked={selectedAnswers[3] === 'B'}
-                  onChange={() => handleOptionChange(3, 'B')}
+                  checked={selectedAnswers[3] === "B"}
+                  onChange={() => handleOptionChange(3, "B")}
                 />
                 <label htmlFor="q3a2">Option B</label>
               </div>
@@ -237,8 +253,8 @@ const StudentPage = ({ details }) => {
                   id="q4a1"
                   name="q4"
                   value="A"
-                  checked={selectedAnswers[4].includes('A')}
-                  onChange={() => handleCheckboxChange(4, 'A')}
+                  checked={selectedAnswers[4].includes("A")}
+                  onChange={() => handleCheckboxChange(4, "A")}
                 />
                 <label htmlFor="q4a1">Option A</label>
               </div>
@@ -248,8 +264,8 @@ const StudentPage = ({ details }) => {
                   id="q4a2"
                   name="q4"
                   value="B"
-                  checked={selectedAnswers[4].includes('B')}
-                  onChange={() => handleCheckboxChange(4, 'B')}
+                  checked={selectedAnswers[4].includes("B")}
+                  onChange={() => handleCheckboxChange(4, "B")}
                 />
                 <label htmlFor="q4a2">Option B</label>
               </div>
@@ -266,8 +282,8 @@ const StudentPage = ({ details }) => {
                   id="q5a1"
                   name="q5"
                   value="A"
-                  checked={selectedAnswers[5].includes('A')}
-                  onChange={() => handleCheckboxChange(5, 'A')}
+                  checked={selectedAnswers[5].includes("A")}
+                  onChange={() => handleCheckboxChange(5, "A")}
                 />
                 <label htmlFor="q5a1">Option A</label>
               </div>
@@ -277,8 +293,8 @@ const StudentPage = ({ details }) => {
                   id="q5a2"
                   name="q5"
                   value="B"
-                  checked={selectedAnswers[5].includes('B')}
-                  onChange={() => handleCheckboxChange(5, 'B')}
+                  checked={selectedAnswers[5].includes("B")}
+                  onChange={() => handleCheckboxChange(5, "B")}
                 />
                 <label htmlFor="q5a2">Option B</label>
               </div>
@@ -286,10 +302,18 @@ const StudentPage = ({ details }) => {
           </div>
         )}
         <div className="navigation">
-          <button className="previous-button" onClick={handlePreviousQuestion} disabled={currentQuestion === 1}>
+          <button
+            className="previous-button"
+            onClick={handlePreviousQuestion}
+            disabled={currentQuestion === 1}
+          >
             Previous
           </button>
-          <button className="next-button" onClick={handleNextQuestion} disabled={currentQuestion === 5}>
+          <button
+            className="next-button"
+            onClick={handleNextQuestion}
+            disabled={currentQuestion === 5}
+          >
             Next
           </button>
         </div>
@@ -299,4 +323,3 @@ const StudentPage = ({ details }) => {
 };
 
 export default StudentPage;
-
