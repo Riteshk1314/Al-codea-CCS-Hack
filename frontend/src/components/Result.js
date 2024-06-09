be982ac80a341e10fc1084d18681a9764653f523
Shreyas Mahajan <142911768+ShreyasMahajann@users.noreply.github.com>
2024-06-09 12:18:01 +0000

    corrected frontend

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -1,20 +1,15 @@
 import React from "react";
-import "./Result.css";
-
 const Result = () => {
   return (
-    <div className="result-container">
-      <h1 className="result-title">Test Results</h1>
-      <div className="result-details">
-        <p>Correct Answers: <span>5</span></p>
-        <p>Incorrect Answers: <span>0</span></p>
-        <p>Unanswered Questions: <span>0</span></p>
-        <p>Score: <span>100%</span></p>
-      </div>
+    <div>
+      <h1>Test Results</h1>
+        <p>Correct Answers: 5</p>
+        <p>Incorrect Answers: 0</p>
+        <p>Unanswered Questions: 0</p>
+        <p>Score: 100%</p>
     </div>
-  );
+    );
 }
 
 export default Result;
-
 
